18ebea4499cadc88c6d08a9aca85f98b4103f08d
Márcio Henrique <marciohr9@gmail.com>
2020-11-30 18:00:59 -0300

    .

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,9 +2,7 @@ import { getManager, getRepository } from "typeorm";
 import User from "../entity/User.entity";
 
 const buscarUser = async (userId: number) => {
-    //const entityManager = getManager();
     const userRepository = getRepository(User);
-    //const user = await entityManager.findOne(User, userId);
     const user = await userRepository.findOne(userId);
     return user;
 }
