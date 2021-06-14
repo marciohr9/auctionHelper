0717856f65a9508a0616a508c74466ac63b0abf3
Márcio Henrique <marciohr9@gmail.com>
2021-06-14 15:04:30 -0300

    ..

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express';
 import UserEntity from '../entity/User.entity';
 import AuthEntity from '../entity/Auth.entity';
-import {Hash, Verify}from '../middlewares/hashValidator.middleware';
+import {Hash, Verify}from '../helpers/hashValidator.helper';
 import 'dotenv/config';
 import { CreateJWT } from '../middlewares/jwtValidator.middleware';
 class AuthController {
@@ -11,25 +11,36 @@ class AuthController {
         let {email, password} = req.body;
         let user: UserEntity;
         let auth: AuthEntity;
-
-        if(!(email && password)){
-            res.status(400).json({authorized: false, mensage: `empity values`});
-        }
         try{
+
+            if(!(email && password)){
+                throw "empty";
+            }
+
             user = await UserEntity.findOneOrFail({where: {email}});
-            auth = await AuthEntity.findOneOrFail({where: {id: user.id}});  
+            auth = await AuthEntity.findOneOrFail({where: {id: user.id}});
             await Verify(password, auth!.pwdHash).then((obj: boolean)=>{
                 if(obj){
                     const token = CreateJWT(user.uuid, user.email);
-                    res.status(201).json({authorized: obj, mensage: `Logged`, token});
+                    res.status(201).json({message: `Logged`, token});
                 }else{
-                    res.status(401).json({authorized: obj, mensage: `Invalid password.`});
+                    throw "invalid-password";
                 }
             }).catch((err: any)=>{
-                res.status(500).json({authorized: false, mensage: 'unespected error on authentication', error_log: err});
+                if(err === "invalid-password"){
+                    res.status(401).json({message: `invalid password. Please try again.`})
+                }else{
+                    res.status(500).json({message: 'unespected error on authentication', error: err});
+                }
             });
-        } catch (error){
-            res.status(401).json({authorized: false, mensage: `invalid user with email ${email}`});
+        } catch (err: any){
+            if(err === "empty"){
+                res.status(400).json({message: `empity values`});
+            }else if(err.name === "EntityNotFound"){
+                res.status(401).json({message: `username ${email} not found. Please try again`});
+            }else{
+                res.status(500).json({message: `unespected error`, error: err});
+            }
         }
     }
     //#
